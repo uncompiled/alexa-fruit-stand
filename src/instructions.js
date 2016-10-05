933bbdaeaf94083043968bae50638e354683b8fa
kazoni <russell.linderer@gmail.com>
2016-10-04 22:07:14 -0500

    Made minor fix to kale

--- src/instructions.js
+++ src/instructions.js
@@ -34,7 +34,7 @@ module.exports = {
   'summer squashes': 'summer squashes should be refrigerated',
   'sweet corn': 'sweet corn should be refrigerated',
   'herbs': 'except for basil, most herbs should be refrigerated',
-  'kale': 'kale should be refrigerated in plastic bag, don\'t wash until ready to use',
+  'kale': 'kale should be refrigerated in a plastic bag, don\'t wash until ready to use',
 
   // Ripen, then refrigerate
   'avocados': 'avocados should be ripened at room temperature, then refrigerated',
