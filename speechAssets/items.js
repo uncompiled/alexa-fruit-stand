4d7467b6ee1ea8fd58cb8f4b634683745f8c739a
Chris Nguyen <chris@uncompiled.org>
2016-11-25 16:09:29 -0500

    Add support for singular and plural forms (#17)
    * Adds better support for pluralization
    * Remove test to check whether item name is used in instruction

--- speechAssets/items.js
+++ speechAssets/items.js
@@ -1,6 +1,9 @@
 const instructions = require('../src/instructions')
 
-for (var key in instructions) {
+/* This script exists to output Custom Slot Types
+ * as part of the Alexa interaction model
+ */
+for (var key in instructions.items) {
   if (key) {
     console.log(key)
   }
--- src/instructions.js
+++ src/instructions.js
@@ -1,4 +1,6 @@
-module.exports = {
+const pluralize = require('pluralize')
+
+const instructions = {
   // Refrigerated items
   'apples': 'apples can be stored at room temperature for one week, but should be refrigerated to keep longer',
   'apricots': 'apricots should be refrigerated',
@@ -16,13 +18,13 @@ module.exports = {
   'endives': 'endives should be stored in the refrigerator',
   'broccoli': 'broccoli should be stored in the refrigerator',
   'brussels sprouts': 'brussels sprouts should be stored in the refrigerator',
-  'cabbage': 'cabbage should be stored in the refrigerator',
+  'cabbages': 'cabbage should be stored in the refrigerator',
   'carrots': 'carrots should be stored in the refrigerator',
-  'cauliflower': 'cauliflower should be stored in the refrigerator',
-  'celery': 'celery should be stored in the refrigerator',
+  'cauliflowers': 'cauliflower should be stored in the refrigerator',
+  'celerys': 'celery should be stored in the refrigerator',
   'cut fruit': 'cut fruit should be refrigerated',
   'cut vegetables': 'cut vegetables should be refrigerated',
-  'green onions': 'green onions should be refrigerated',
+  'green onion': 'green onions should be refrigerated',
   'leafy vegetables': 'leafy vegetables should be refrigerated',
   'leeks': 'leeks should be refrigerated',
   'lettuce': 'lettuce should be refrigerated',
@@ -37,43 +39,54 @@ module.exports = {
   'kale': 'kale should be refrigerated in a plastic bag, don\'t wash until ready to use',
 
   // Ripen, then refrigerate
-  'avocados': 'avocados should be ripened at room temperature, then refrigerated',
+  'avocado': 'avocados should be ripened at room temperature, then refrigerated',
   'kiwifruit': 'kiwifruit should be ripened at room temperature, then refrigerated',
   'kiwi': 'kiwi should be ripened at room temperature, then refrigerated',
-  'nectarines': 'nectarines should be ripened at room temperature, then refrigerated',
-  'peaches': 'peaches should be ripened at room temperature, then refrigerated',
-  'pears': 'pears should be ripened at room temperature, then refrigerated',
-  'plums': 'plums should be ripened at room temperature, then refrigerated',
-  'plumcots': 'plumcots should be ripened at room temperature, then refrigerated',
+  'nectarine': 'nectarines should be ripened at room temperature, then refrigerated',
+  'peach': 'peaches should be ripened at room temperature, then refrigerated',
+  'pear': 'pears should be ripened at room temperature, then refrigerated',
+  'plum': 'plums should be ripened at room temperature, then refrigerated',
+  'plumcot': 'plumcots should be ripened at room temperature, then refrigerated',
 
   // Store at room temperature
   'bananas': 'bananas should be stored at room temperature',
   'grapefruit': 'grapefruit should be stored at room temperature',
-  'lemons': 'lemons should be stored at room temperature',
-  'limes': 'limes should be stored at room temperature',
-  'mandarins': 'mandarins should be stored at room temperature',
-  'mangoes': 'mangoes should be stored at room temperature',
-  'melons': 'melons should be stored at room temperature',
-  'oranges': 'oranges should be stored at room temperature',
-  'papayas': 'papayas should be stored at room temperature',
-  'persimmons': 'persimmons should be stored at room temperature',
+  'lemon': 'lemons should be stored at room temperature',
+  'lime': 'limes should be stored at room temperature',
+  'mandarin': 'mandarins should be stored at room temperature',
+  'mango': 'mangoes should be stored at room temperature',
+  'melon': 'melons should be stored at room temperature',
+  'orange': 'oranges should be stored at room temperature',
+  'papaya': 'papayas should be stored at room temperature',
+  'persimmon': 'persimmons should be stored at room temperature',
   'pineapple': 'pineapple should be stored at room temperature',
-  'plantains': 'plantains should be stored at room temperature',
-  'pomegranates': 'pomegranates should be stored at room temperature',
-  'watermelons': 'watermelons should be stored at room temperature',
+  'plantain': 'plantains should be stored at room temperature',
+  'pomegranate': 'pomegranates should be stored at room temperature',
+  'watermelon': 'watermelons should be stored at room temperature',
 
   'basil': 'basil should be stored at room temperature. for best results, store in water',
-  'cucumbers': 'cucumbers should be stored at room temperature. cucumbers can be kept in the refrigerator if consumed within 1 to 3 days',
+  'cucumber': 'cucumbers should be stored at room temperature. cucumbers can be kept in the refrigerator if consumed within 1 to 3 days',
   'eggplant': 'eggplant should be stored at room temperature. eggplant can be kept in the refrigerator if consumed within 1 to 3 days',
-  'peppers': 'peppers should be stored at room temperature. peppers can be kept in the refrigerator if consumed within 1 to 3 days',
-  'onions': 'onions should be stored in a well ventilated area in the pantry',
+  'pepper': 'peppers should be stored at room temperature. peppers can be kept in the refrigerator if consumed within 1 to 3 days',
+  'onion': 'onions should be stored in a well ventilated area in the pantry',
   'garlic': 'garlic should be stored in a well ventilated area in the pantry',
-  'potatoes': 'potatoes should be stored in a well ventilated area in the pantry.  protect potatoes from light to avoid greening.',
-  'sweet potatoes': 'sweet potatoes should be stored in a well ventilated area in the pantry',
+  'potato': 'potatoes should be stored in a well ventilated area in the pantry.  protect potatoes from light to avoid greening.',
+  'sweet potato': 'sweet potatoes should be stored in a well ventilated area in the pantry',
 
   'pumpkins': 'pumpkins should be stored at room temperature',
   'ginger': 'ginger should be stored at room temperature',
   'jicama': 'jicama should be stored at room temperature',
   'tomatoes': 'tomatoes should be stored at room temperature',
-  'winter squashes': 'winter squashes should be stored at room temperature'
+  'winter squash': 'winter squashes should be stored at room temperature'
+}
+
+module.exports = {
+  /* find returns instructions for a given item
+  * @param {string} itemName - item to check for instructions
+  * @returns {string} string containing speech text
+  */
+  find: function (itemName) {
+    return instructions[itemName] || instructions[pluralize(itemName)]
+  },
+  items: instructions
 }
