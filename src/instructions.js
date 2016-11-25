286ee62946a1bd5673f9a122eb713a9d9ae855aa
uncompiled <chris@uncompiled.org>
2016-11-25 16:47:02 -0500

    Add instructions for turnips
    Closes #9 using information from http://www.stilltasty.com/fooditems/index/18595

--- src/instructions.js
+++ src/instructions.js
@@ -39,6 +39,7 @@ const instructions = {
   'herbs': 'except for basil, most herbs should be refrigerated',
   'kale': 'kale should be refrigerated in a plastic bag, don\'t wash until ready to use',
   'green beans': 'green beans should be refrigerated in a plastic bag. do not wash until ready to use',
+  'turnips': 'raw turnips should be refrigerated in a plastic bag until ready to use',
 
   // Ripen, then refrigerate
   'avocados': 'avocados should be ripened at room temperature, then refrigerated',
