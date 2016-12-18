16b704e2dbec1ad2f6681d15a69ec553653ff43c
uncompiled <chris@uncompiled.org>
2016-12-18 08:25:58 -0500

    Add instructions for parsley
    Closes #21 by adding instructions from SimplyRecipes
    
    http://www.simplyrecipes.com/recipes/how_to_store_parsley_cilantro_and_other_fresh_herbs/

--- src/instructions.js
+++ src/instructions.js
@@ -48,6 +48,7 @@ const instructions = {
   'zucchinis': 'zucchinis should be refrigerated in a plastic bag.  do not wash until ready to use.',
   'dragon fruit': 'dragon fruit will keep on the counter for a few days. if you want to keep it longer than that, store it in a plastic bag in the refrigerator',
   'cilantro': 'cilantro should be refrigerated. for best results, place stems in a jar of water with a plastic sandwich bag loosely covering the leaves',
+  'parsley': 'parsley can be stored at room temperature or refrigerated. for best results, place stems in a jar of water with a plastic sandwich bag loosely covering the leaves',
 
   // Ripen, then refrigerate
   'avocados': 'avocados should be ripened at room temperature, then refrigerated',
